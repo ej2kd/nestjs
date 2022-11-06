48275b405f2855c54ba1332c958eb77daf208b5c
ej2kd <eiji.tsukada9415@gmail.com>
2022-11-07 00:57:21 +0900

    custom interceptor approach

---
+++ prod/mycv/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): {}
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
+    // Run something before a request is handled by the request handler
+    console.log('I am running before the handler', context);
+    
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      })
+    );
+  }
+}
