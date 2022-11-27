ae4bd43bf36e18901a7c045bda63a7ed91de4a9c
ej2kd <eiji.tsukada9415@gmail.com>
2022-11-27 15:14:35 +0900

    add signup unittest to auth.service.spec

--- prod/mycv/src/users/auth.service.spec.ts
+++ prod/mycv/src/users/auth.service.spec.ts
@@ -31,4 +31,13 @@ describe('AuthService', () => {
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
+
+  it('create a new user with a salted and hashed password', async () => {
+    const user = await service.signup('asdf@asdf.com', 'asdf');
+    
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
 });
