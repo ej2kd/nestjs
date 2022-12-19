22bc3474162b958b0250b5b1fab2e979fa8001dd
ej2kd <eiji.tsukada9415@gmail.com>
2022-12-20 00:37:02 +0900

    implement users.controller.spec

--- prod/mycv/src/users/users.controller.spec.ts
+++ prod/mycv/src/users/users.controller.spec.ts
@@ -27,7 +27,9 @@ describe('UsersController', () => {
     };
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => ()
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -65,5 +67,16 @@ describe('UsersController', () => {
   it('findUser throws an error if user with the given id is not found', async () => {
     fakeUsersService.findOne = () => null;
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'asdf@asdf.com', password: 'asdf' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
--- prod/mycv/src/users/users.controller.ts
+++ prod/mycv/src/users/users.controller.ts
@@ -13,13 +13,13 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './users.entity';
 import { UseGuards } from '@nestjs/common';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
