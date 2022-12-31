ef44555328058c6827ef93d4fda98e0bca15c025
ej2kd <eiji.tsukada9415@gmail.com>
2023-01-01 00:47:35 +0900

    adding in report approval

--- prod/mycv/src/reports/reports.service.ts
+++ prod/mycv/src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './reports.entity';
@@ -13,6 +13,16 @@ export class ReportsService {
     const report = this.repo.create(reportDto);
     report.user = user;
 
+    return this.repo.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.approved = approved;
     return this.repo.save(report);
   }
 }
