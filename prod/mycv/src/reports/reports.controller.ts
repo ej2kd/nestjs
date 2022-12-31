38ac0ed3d9b039ff22ba585569525256cbd73ff4
ej2kd <eiji.tsukada9415@gmail.com>
2023-01-01 00:25:41 +0900

    formatting the report response

--- prod/mycv/src/reports/reports.controller.ts
+++ prod/mycv/src/reports/reports.controller.ts
@@ -4,6 +4,8 @@ import { CreateReportDto } from './dtos/create-report.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/users.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,6 +13,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard) // Only logged in users can create reports
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
